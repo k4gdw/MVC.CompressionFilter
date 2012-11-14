a0a5b910aa12b62ec2cb8cfcb2b8852916498f57
Bryan Johns <bryan.johns@mh.alabama.gov>
2012-11-14 09:35:17 -0600

    Added the CompressionFilter project.
    new file:   CompressionFilter/CompressionFilter.vb
    new file:   CompressionFilter/CompressionFilter.vbproj
    new file:   CompressionFilter/Extensions.vb
    new file:   CompressionFilter/My Project/Application.Designer.vb
    new file:   CompressionFilter/My Project/Application.myapp
    new file:   CompressionFilter/My Project/AssemblyInfo.vb
    new file:   CompressionFilter/My Project/Resources.Designer.vb
    new file:   CompressionFilter/My Project/Resources.resx
    new file:   CompressionFilter/My Project/Settings.Designer.vb
    new file:   CompressionFilter/My Project/Settings.settings
    modified:   MVC.CompressionFilter.sln

---
+++ CompressionFilter/CompressionFilter.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Web
+Imports System.Web.Mvc
+Imports System.IO.Compression
+
+Public Class CompressionFilter
+	Inherits ActionFilterAttribute
+
+	Public Overrides Sub OnActionExecuting(filterContext As ActionExecutingContext)
+		Dim request As HttpRequestBase = filterContext.HttpContext.Request
+		Dim acceptEncoding As String = request.Headers("Accept-Encoding")
+		If acceptEncoding.IsEmptyOrNull() Then
+			Return
+		End If
+		acceptEncoding = acceptEncoding.ToUpperInvariant()
+		Dim response As HttpResponseBase = filterContext.HttpContext.Response
+		If acceptEncoding.Contains("GZIP") Then
+			response.AppendHeader("Content-encoding", "gzip")
+			response.Filter = New GZipStream(response.Filter, CompressionMode.Compress)
+		ElseIf acceptEncoding.Contains("DEFLATE") Then
+			response.AppendHeader("Content-encoding", "deflate")
+			response.Filter = New DeflateStream(response.Filter, CompressionMode.Compress)
+		End If
+	End Sub
+
+
+End Class
---
+++ CompressionFilter/Extensions.vb
@@ -0,0 +1,40 @@
+﻿Imports System.Runtime.CompilerServices
+
+Module Extensions
+
+	''' <summary>
+	''' Determines whether [the specified a string] [is empty or null] .
+	''' </summary>
+	''' <param name="aString">A string.</param>
+	''' <returns>
+	''' <c>true</c> if [is empty or null] [the specified a string]; otherwise, <c>false</c>.
+	''' </returns>
+	''' <remarks>
+	''' <para>
+	''' I created this extension method so that I could use this functionality in a fluent manner
+	''' like so:
+	''' <code>
+	''' Dim x As String
+	''' If x.IsEmptyOrNull Then
+	''' 'do stuff
+	''' End If
+	''' </code>
+	''' instead of having to do it like this:
+	''' <code>
+	''' Dim x as String
+	''' If String.IsNullOrEmpty(x) or String.IsNullOrWhiteSpace(x) Then
+	''' ' do stuff
+	''' End If
+	''' </code>
+	''' </para>
+	''' <para>
+	''' Created:  5/23/2012 at 9:10 AM<br />
+	''' By:       bryan.johns
+	''' </para>
+	''' </remarks>
+	<Extension()>
+	Public Function IsEmptyOrNull(aString As String) As Boolean
+		Return String.IsNullOrEmpty(aString) OrElse String.IsNullOrWhiteSpace(aString)
+	End Function
+
+End Module
